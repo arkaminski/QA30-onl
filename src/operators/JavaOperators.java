57e5d394348e18875fc094a331a6bb92e97a9346
unknown <arturkaminski0612@gmail.com>
2025-03-05 17:27:55 +0100

    Update documents with Loops and Operators

---
+++ src/operators/JavaOperators.java
@@ -0,0 +1,8 @@
+public class JavaOperators{
+    // One-line comment
+    // Entry-point, точка входа
+    public static void main(String[] args){
+        // Точка входа для всего кода
+        // Method call вызов метода
+
+    }
