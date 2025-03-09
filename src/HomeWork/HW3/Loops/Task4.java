b78833fd3115a72100177d729f962f3a401df27e
unknown <arturkaminski0612@gmail.com>
2025-03-09 17:11:58 +0100

    Loops - task4

---
+++ src/HomeWork/HW3/Loops/Task4.java
@@ -0,0 +1,15 @@
+package HomeWork.HW3.Loops;
+
+public class Task4 {
+    public static void main(String [] args) {
+        getNumbers();
+    }
+        public static void getNumbers(){
+            int number = 7;
+            while (number <=98) {
+                System.out.println(number);
+                number += 7;
+            }
+        }
+    }
+
