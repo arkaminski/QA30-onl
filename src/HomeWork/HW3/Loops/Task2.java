fd0758b114ad2f2484b893f42c7dce981bb20945
unknown <arturkaminski0612@gmail.com>
2025-03-09 16:48:21 +0100

    Loops - task2

---
+++ src/HomeWork/HW3/Loops/Task2.java
@@ -0,0 +1,13 @@
+package HomeWork.HW3.Loops;
+
+public class Task2 {
+    public static void main(String[] args){
+        getNumbers();
+    }
+    public static void getNumbers(){
+        for (int number = 5; number > 0; number--) {
+            System.out.println(number);
+        }
+    }
+}
+
