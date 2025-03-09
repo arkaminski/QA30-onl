56551eb3c287e60a964fc5e7ded5aaed9dff1601
unknown <arturkaminski0612@gmail.com>
2025-03-09 17:31:28 +0100

    Loops - task6

---
+++ src/HomeWork/HW3/Loops/Task6.java
@@ -0,0 +1,17 @@
+package HomeWork.HW3.Loops;
+
+public class Task6 {
+    public static void main(String [] args){
+        outputNumbers();
+    }
+
+    public static void outputNumbers(){
+        for (int i = 10; i <= 20; i++){
+            int Square = i * i;
+            System.out.println(Square);
+        }
+
+
+
+    }
+}
