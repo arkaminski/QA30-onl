c233e96019c8094d77ab6a91af78ac8ca44a8a81
unknown <arturkaminski0612@gmail.com>
2025-03-09 17:51:10 +0100

    Additional task - task1

--- src/HomeWork/HW3/AdditionalTask/Task1.java
+++ src/HomeWork/HW3/AdditionalTask/Task1.java
@@ -1,4 +1,27 @@
 package HomeWork.HW3.AdditionalTask;
 
 public class Task1 {
+    public static void main(String [] args) {
+        outputFibonacci();
+    }
+    public static void outputFibonacci() {
+        System.out.println("The first 11 terms of the Fibonacci sequence : ");
+        int previous = 0;
+        int now = 1;
+
+        for (int i = 1; i <= 11; i++){
+            System.out.println(previous);
+            int next = previous + now;
+            previous = now;
+            now = next;
+
+        }
+    }
+
+
+
+
+
+
+
 }
