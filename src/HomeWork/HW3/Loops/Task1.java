4890ba3ef82ef85856b2e13ecad7ca8e8b9541a5
unknown <arturkaminski0612@gmail.com>
2025-03-09 16:42:48 +0100

    Loops - task1

--- src/HomeWork/HW3/Loops/Task1.java
+++ src/HomeWork/HW3/Loops/Task1.java
@@ -1,4 +1,17 @@
 package HomeWork.HW3.Loops;
 
+
 public class Task1 {
+    public static void main(String[] args) {
+        getOddsNumbers();
+    }
+
+    public static void getOddsNumbers(){
+            for (int number = 1; number <= 99; number++) {
+                if ((number % 2) == 0) {
+                    continue;
+                }
+                System.out.println(number);
+            }
+    }
 }
