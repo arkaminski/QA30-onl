83ed8a01b1167a21bf7adb7f05e454a4f0e8cee8
unknown <arturkaminski0612@gmail.com>
2025-03-12 17:13:15 +0100

    Lesson_4

--- src/HomeWork/HW3/AdditionalTask/Task1.java
+++ src/HomeWork/HW3/AdditionalTask/Task1.java
@@ -2,26 +2,14 @@ package HomeWork.HW3.AdditionalTask;
 
 public class Task1 {
     public static void main(String [] args) {
-        outputFibonacci();
+        getMultiplicationTable();
     }
-    public static void outputFibonacci() {
-        System.out.println("The first 11 terms of the Fibonacci sequence : ");
-        int previous = 0;
-        int now = 1;
-
-        for (int i = 1; i <= 11; i++){
-            System.out.println(previous);
-            int next = previous + now;
-            previous = now;
-            now = next;
-
+    public static void  getMultiplicationTable() {
+        for (int i = 1; i <= 10; i++) {
+            for (int a = 1; a <= 10; a++) {
+                System.out.print(" " + (i * a) + " ");
+            }
+            System.out.println();
         }
     }
-
-
-
-
-
-
-
 }
